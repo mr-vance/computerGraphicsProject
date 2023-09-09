79d2876a8482b99bf660a6b75aa12d83c56f1753
Vance <muchongo.vance@gmail.com>
2023-09-09 14:52:41 +0200

    implemented #18 game not playable on mobile

--- help.js
+++ help.js
@@ -14,3 +14,24 @@ document.addEventListener('DOMContentLoaded', function () {
         popupCard.style.display = 'none';
     });
 });
+
+// Check if the screen width is less than 868 pixels (adjust this value as needed)
+const isMobile = window.innerWidth < 868;
+
+// Get references to the mobile message and overlay elements
+const mobileMessage = document.getElementById('mobile-message');
+const overlay = document.getElementById('overlay');
+
+// Show the message and overlay if it's a mobile device
+if (isMobile) {
+    mobileMessage.style.display = 'block';
+    overlay.style.display = 'block';
+    overlay.classList.add('active'); // Enable interactions with overlay
+} else {
+    // Initialize your Three.js game if it's not a mobile device
+    // Your game initialization code goes here
+    overlay.classList.remove('active'); // Disable interactions with overlay
+}
+
+
+
